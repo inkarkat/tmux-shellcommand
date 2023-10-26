58fe84f95d28d71da2a33e8e93704365af78d8d8
Ingo Karkat <swdev@ingo-karkat.de>
2023-10-26 09:41:36 +0200

    Style: Named function arguments, line breaks, test with -n, naming

--- scripts/shellcommand.sh
+++ scripts/shellcommand.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-commandHistory="${1?}"; shift
+commandHistoryFilespec="${1?}"; shift
 outputHistory="${1?}"; shift
 if [ $# -eq 0 ]; then
     shellcommand="$(tmux show-options -gv @queried_command)"
@@ -13,18 +13,24 @@ fi
 
 capturedOutput="$(eval "$shellcommand" 2>&1)"
 
-if [ -n "$commandHistory" -o -n "$outputHistory" ]; then
+if [ -n "$commandHistoryFilespec" -o -n "$outputHistory" ]; then
     escapeNewline()
     {
 	sed ':a; s/\\/\\\\/g; x; G; 1s/\n//; s/\n/\\n/; h; N; s/.*\n//; ta' <<<"${1:?}"
     }
     storeHistory()
     {
-	if [ -n "$1" -a -n "$2" ]; then
-	    printf '%s\n' "$(escapeNewline "$2")" >> "$1"
-	    if [ "$history_size" ]; then
+	local filespec="${1?}"; shift
+	local output="${1?}"; shift
+
+	if [ -n "$filespec" -a -n "$output" ]; then
+	    printf '%s\n' "$(escapeNewline "$output")" >> "$filespec"
+
+	    if [ -n "$history_size" ]; then
 		TMPFILE="$(mktemp --tmpdir "$(basename -- "$0")-XXXXXX" 2>/dev/null || echo "${TMPDIR:-/tmp}/$(basename -- "$0").$$$RANDOM")"
-		tail -n "$history_size" "$1" > "$TMPFILE" && cp --force -- "$TMPFILE" "$1" && rm -- force -- "$TMPFILE"
+		tail -n "$history_size" "$1" > "$TMPFILE" \
+		    && cp --force -- "$TMPFILE" "$filespec" \
+		    && rm -- force -- "$TMPFILE"
 	    fi
 	fi
     }
@@ -33,7 +39,7 @@ if [ -n "$commandHistory" -o -n "$outputHistory" ]; then
     history_size="$(tmux show-option -gv '@shellcommand_history_size' 2>/dev/null)"
     history_size="${history_size:-$default_history_size}"
 
-    storeHistory "$commandHistory" "$shellcommand"
+    storeHistory "$commandHistoryFilespec" "$shellcommand"
     storeHistory "$outputHistory" "$capturedOutput"
 fi
 
